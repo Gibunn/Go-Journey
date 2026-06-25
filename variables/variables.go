42b2c0dc3374c66bd863062782455ae31143d0f4
MOVRA <abgibun@gmail.com>
2026-06-25 09:52:20 +0700

    learning hello world, values, variables

---
+++ variables/variables.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "Init"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	var f string
+	fmt.Println(f)
+
+	var g bool
+	fmt.Println(g)
+
+	var h float32
+	fmt.Println(h)
+
+	i := "Strawberries"
+	fmt.Println(i)
+}
